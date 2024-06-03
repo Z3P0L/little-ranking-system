4ee2d8cedcb6bac6070545609506bbe38e8b89a7
z3p0l <worldtico@gmail.com>
2024-06-02 19:11:48 -0500

    add template for app pages

---
+++ meta.php
@@ -0,0 +1,4 @@
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/lib/css/normalize.css">
+    <link rel="stylesheet" href="/lib/css/styles.css">
---
+++ nav.php
@@ -0,0 +1,18 @@
+    <nav>
+    	<figure>
+    		<a href="/">
+    			<img src="/lib/img/logo.png" alt="logo" width="150px">
+    		</a>
+    	</figure>
+    	<ul>
+    		<li>
+    			<a href="/create_task.php">Tareas</a>
+    		</li>
+    		<li>
+    			<a href="/">Ranking</a>
+    		</li>
+    		<li>
+    			<a href="/check_tasks.php">Mi perfil</a>
+    		</li>
+    	</ul>
+    </nav>
